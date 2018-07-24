04be9376d6c18016268b1d788bff6c58fdd3625d
Владимир <v.merkel778@gmail.com>
2018-07-24 10:57:06 +0700

    Лаба5 поправлен выбор действий в текстовом редакторе

--- Bashprog/lab5/textpad.sh
+++ Bashprog/lab5/textpad.sh
@@ -38,61 +38,76 @@ case $? in
 				;; 
 		esac 
 		;; 
-	1) 
-		FILE_GET="`$DIALOG --stdout --title "Введите название файла" --inputbox "Поле ввода" 16 51`"
-		
-		case $? in 
+	1)
+		$DIALOG --title "Текстовый редакторш" --clear \
+				--yesno "Создать новый файл?" 10 40
+		case $? in
 			0)
-				DIR_SELECT="`$DIALOG --stdout --title "Введите путь куда сохранить файл" --dselect $HOME/ 10 40`"
-				case $? in
+			FILE_GET="`$DIALOG --stdout --title "Введите название файла" --inputbox "Поле ввода" 16 51`"
+		
+			case $? in 
 				0)
-					FILE_NEW="$DIR_SELECT$FILE_GET"
-					touch "$FILE_NEW"
-					echo "$FILE_NEW"
-					TEXT_CHANGE="`$DIALOG --stdout --title "Редактирование" --editbox "$FILE_NEW" 40 100`"
+					DIR_SELECT="`$DIALOG --stdout --title "Введите путь куда сохранить файл" --dselect $HOME/ 10 40`"
 					case $? in
-						0)
-							clear 
-							echo "Изменения сохранены" 
-							echo "$TEXT_CHANGE">"$FILE_NEW"
-							exit 0 
-							;; 
-						1) 
-							clear 
-							echo "Изменения не сохранены" 
-							exit 0 
-							;; 
-						255) 
-							clear
-							echo "Выход" 
-							;; 
-					esac 
+					0)
+						FILE_NEW="$DIR_SELECT$FILE_GET"
+						touch "$FILE_NEW"
+						echo "$FILE_NEW"
+						TEXT_CHANGE="`$DIALOG --stdout --title "Редактирование" --editbox "$FILE_NEW" 40 100`"
+						case $? in
+							0)
+								clear 
+								echo "Изменения сохранены" 
+								echo "$TEXT_CHANGE">"$FILE_NEW"
+								exit 0 
+								;; 
+							1) 
+								clear 
+								echo "Изменения не сохранены" 
+								exit 0 
+								;; 
+							255) 
+								clear
+								echo "Выход" 
+								;; 
+						esac 
+					;;
+					1)
+						clear
+						echo "Не было определено место хранения файла"
+						exit 0
+					;;
+					255)
+						clear
+						echo "Нажата клавиша ESC." 
+						exit 0 
+					;;
+					esac
 				;;
-				1)
-					clear
-					echo "Не было определено место хранения файла"
-					exit 0
-				;;
-				255)
-					clear
+				1) 
+					clear 
+					echo "Отказ от ввода." 
+					exit 0 
+				;; 
+				255) 
+					clear 
 					echo "Нажата клавиша ESC." 
 					exit 0 
-				;;
-				esac
+				;; 
+				esac 
+			clear
 			;;
-			1) 
+			1)
 				clear 
-				echo "Отказ от ввода." 
-				exit 0 
-			;; 
-			255) 
-				clear 
-				echo "Нажата клавиша ESC." 
-				exit 0 
-			;; 
-			esac 
-		clear
-		;; 
+				echo "Ничего не было выбрано"
+				exit 0
+			;;
+			255)
+				clear
+				echo "Нажата клавиша ESC."
+				exit 0
+		esac 
+		;;
 	255) 
 		clear 
 		echo "Выход" 
