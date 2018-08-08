ecad0d7049537619c0d53926156698bdc13cc6ec
Владимир <v.merkel778@gmail.com>
2018-08-08 16:15:19 +0700

    Лаба11 зачатки сервера

---
+++ Cprog/lab11/server.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<wait.h>
+#include<time.h>
+#include<math.h>
+
+//Вариант 10
+
+struct
+{
+	pthread_mutex_t mutex;
+	int x;
+	int y;
+	int end_x;
+	int end_y;
+	pthread_t pthread_scout;
+	int step;
+	int flag_end;
+}
+shared = {
+	PTHREAD_MUTEX_INITIALIZER
+};
+
+struct
+{
+	pthread_mutex_t mutex;
+	int *x;
+	int *y;
+	int size_mass;
+}
+shared2 = {
+	PTHREAD_MUTEX_INITIALIZER
+};
+
+int size_fild;
+
+struct DATA
+{
+	int x;
+	int y;
+};
+
+typedef struct DATA coord_loc;
+
+struct DATA_MAP
+{
+	int size_fild;
+	int scouts;
+};
+typedef struct DATA_MAP data_map;
+
+void input(int *scouts)
+{
+	printf("Введите размер карты(квадратная)\n");
+	scanf("%d", &size_fild);
+	printf("Введите количество разведчиков\n");
+	scanf("%d", scouts);
+}
+
+void *thread_func_map(void *arg)
+{
+	return arg;
+}
+
+void *thread_func_scout(void *arg)
+{
+	return arg;
+}
+
+int main()
+{
+	srand(getpid());
+	system("clear");
+	int scouts;
+	int result;
+	input(&scouts);
+	pthread_t pthread_scout[scouts];
+	pthread_t pthread_map;
+	coord_loc start_loc;
+	start_loc.x = 1 + rand()%(size_fild-2);
+	start_loc.y = 1 + rand()%(size_fild-2);
+	data_map data_init_map;
+	data_init_map.size_fild = size_fild;
+	data_init_map.scouts = scouts;
+	void *status[scouts];
+	void *status_map;
+	int id_pthread[scouts];
+	result = pthread_create(&pthread_map, NULL, thread_func_map, &data_init_map);
+	if(result != 0)
+	{
+		perror("Создание треда карты не удалось\n");
+		return EXIT_FAILURE;
+	}
+	for(int i = 0; i<scouts; i++)
+	{
+		result = pthread_create(&pthread_scout[i], NULL, thread_func_scout, &start_loc);
+		if(result != 0)
+		{
+			perror("Создание треда не удалось\n");
+			return EXIT_FAILURE;
+		}
+	}
+	for(int i = 0; i < scouts; i++)
+	{
+		result = pthread_join(pthread_scout[i], &status[i]);
+		if(result != 0)
+		{
+			perror("Вход в тред не удалось\n");
+			return EXIT_FAILURE;
+		}
+	}
+	result = pthread_join(pthread_map, &status_map);
+	if(result != 0)
+	{
+		perror("Вход в тред не удалось\n");
+		return EXIT_FAILURE;
+	}
+	printf("Done\n");
+	return EXIT_SUCCESS;
+}
+
+
