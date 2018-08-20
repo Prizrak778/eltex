e41843f467d9f3fc4d373701b9a97ddb4aa5c54d
Владимир <v.merkel778@gmail.com>
2018-08-20 16:29:04 +0700

    Курсовая инициализация сервера и клиенты 2 типов

---
+++ Cprog/coure_work/client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define port_ser 50000
+#define ip_serv "192.168.0.118"
+
+//Client v
+struct DATA_send
+{
+	int client_v;
+};
+
+int main()
+{
+	int socket_tcp, socket_udp;
+	struct sockaddr_in st_addr_tcp, st_addr_udp;
+	struct DATA_send *data_send = (struct DATA_send *)malloc(sizeof(struct DATA_send));
+	data_send->client_v = 1;
+	if((socket_tcp = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Клиент: не смог создать сокет\n");
+		sleep(5);
+		exit(1);
+	}
+	printf("Клиент: создал сокет\n");
+	st_addr_tcp.sin_family = AF_INET;
+	st_addr_tcp.sin_port = htons(port_ser);
+	st_addr_tcp.sin_addr.s_addr = inet_addr(ip_serv);
+	if(connect(socket_tcp, (struct sockaddr *)&st_addr_tcp, sizeof(st_addr_tcp))== -1)
+	{
+		printf("Клиент: ошибка присоединении к серверу\n");
+		close(socket_tcp);
+		sleep(5);
+		exit(1);
+	}
+	//Отправить версию клиента по tpc
+	printf("Клиент: приконектился к серверу\n");
+	printf("size=%d\n", sizeof(struct DATA_send));
+	sleep(5);
+	if(send(socket_tcp, data_send, sizeof(struct DATA_send), MSG_WAITALL) == -1)
+	{
+		printf("Клиент: новые координаты не отправлены\n");
+		close(socket_tcp);
+		sleep(5);
+		exit(1);
+	}
+	close(socket_tcp);
+	printf("end\n");
+	sleep(5);
+	free(data_send);
+	return 0;
+}
+
+
---
+++ Cprog/coure_work/server.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+
+#define port_server 50000
+
+//server
+struct ThreadArgs
+{
+	int clntSock;
+};
+
+struct DATA_recv
+{
+	int client_v;
+};
+
+void *Threadclient1(void *arg)
+{
+	int clntSock;
+	//pthread_detach(pthread_self());
+	clntSock = ((struct ThreadArgs *) arg) -> clntSock;
+	printf("Сервер: клиент версии 1 обнаружен, номер сокета = %d\n", clntSock);
+	free(arg);
+	return NULL;
+}
+void *Threadclient2(void *arg)
+{
+	int clntSock;
+	//pthread_detach(pthread_self());
+	clntSock = ((struct ThreadArgs *) arg) -> clntSock;
+	printf("Сервер: клиент версии 1 обнаружен, номер сокета = %d\n", clntSock);
+	free(arg);
+	return NULL;
+}
+
+
+int main(int argc, char* argv[])
+{
+	int* client_v;
+	int servSock;
+	int clntSock;
+	struct sockaddr_in st_addr;
+	struct ThreadArgs *threadclient;
+	unsigned short ServPort;
+	struct DATA_recv* data_recv = (struct DATA_recv *) malloc(sizeof(struct DATA_recv));
+	//ServPort = atoi(argv[1]);
+	st_addr.sin_family = AF_INET;
+	st_addr.sin_addr.s_addr = INADDR_ANY;
+	st_addr.sin_port = htons(port_server);
+	pthread_t threadID;
+	if(( servSock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Сервер: ошибка при создании сокета\n");
+		close(servSock);
+		exit(1);
+	}
+	printf("Сервер: создал сокет\n");
+	if((bind(servSock, (struct sockaddr *)&st_addr, sizeof(st_addr))) == -1)
+	{
+		printf("Сервер: ошибка в bind\n");
+		close(servSock);
+		exit(1);
+	}
+	printf("Сервер: bind прошёл успешно\n");
+	if(listen(servSock, 10) == -1)
+	{
+		printf("Сервер: ошибка в попытке прослушать порт\n");
+		close(servSock);
+		exit(1);
+	}
+	printf("Сервер: сокет прослушывается\n");
+	while(1)
+	{
+		if((clntSock = accept(servSock, NULL, NULL)) == -1)
+		{
+			printf("Сервер: aксепт фэйл\n");
+		}
+		else
+		{
+			if((threadclient = (struct ThreadArgs *) malloc(sizeof(struct ThreadArgs))) == NULL)
+			{
+				printf("Сервер: malloc is fail\n");
+			}
+			threadclient -> clntSock = clntSock;
+			if(recv(clntSock, data_recv, sizeof(struct DATA_recv), MSG_DONTROUTE))
+			{
+				printf("recv_data=%d\n", data_recv->client_v);
+				if(data_recv->client_v == 1)
+				{
+					if(pthread_create(&threadID, NULL, Threadclient1, (void *) threadclient) != 0)
+					{
+						printf("Сервер: ошибка при создание треда\n");
+					}
+				}
+				else if(data_recv->client_v == 2)
+				{
+					if(pthread_create(&threadID, NULL, Threadclient2, (void *) threadclient) != 0)
+					{
+						printf("Сервер: ошибка при создание треда\n");
+					}
+				}
+				else
+				{
+					printf("Сервер: клиент не распознан\n");
+				}
+			}
+			else
+			{
+				printf("Сервер: ошибка при принятии сообщения\n");
+			}
+		}
+	}
+}
